541778ec3dfcae75b45d727b7a05649981bff31b
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-04-02 17:23:37 +0200

    remove form tag & add favicon

--- components/AccountManager.tsx
+++ components/AccountManager.tsx
@@ -1,28 +1,22 @@
 const AccountManager = () => {
   return (
     <div className="accountManager">
-      <form>
-        <div className="container ps-0">
-          <div className="row">
-            <div className="col-6">
-              <select
-                name="accountType"
-                id="accountType"
-                className="form-select"
-              >
-                <option value="binance">ETH wallet address</option>
-                <option value="binance">Binance API key</option>
-              </select>
-            </div>
-            <div className="col-4">
-              <input type="text" className="form-control" />
-            </div>
-            <div className="col-2">
-              <button className="btn btn-success">+</button>
-            </div>
+      <div className="container ps-0">
+        <div className="row">
+          <div className="col-6">
+            <select name="accountType" id="accountType" className="form-select">
+              <option value="binance">ETH wallet address</option>
+              <option value="binance">Binance API key</option>
+            </select>
+          </div>
+          <div className="col-4">
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-2">
+            <button className="btn btn-success">+</button>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
