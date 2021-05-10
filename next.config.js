396c6e213c11912bde8dd4613fb9402555742d86
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-05-10 07:23:01 +0200

    add kucoin env variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+const kucoin = require('./clientEnv');
+
+const config = {
   future: {
     webpack5: true,
   },
+  env: {
+    kucoinKey: kucoin.key,
+    kucoinSecret: kucoin.secret,
+    kucoinPasshrase: kucoin.passhrase,
+  },
 }
+
+module.exports = config
