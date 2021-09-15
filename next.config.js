8857759d12b86af24b27ee83ee3ae12434afbce6
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-09-15 18:50:22 +0200

    update packages

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const clientEnvVariables = require("./clientEnv");
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   env: {
     ethNodeURL: clientEnvVariables.ethNodeURL
   },
