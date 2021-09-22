ca3dcf62b585b5b54d48600d364876dbfcedd9eb
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-09-22 12:07:17 +0200

    display current price of a token

--- components/TokenComp.tsx
+++ components/TokenComp.tsx
@@ -5,11 +5,13 @@ const TokenComp = (props: { token: Token }) => {
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
       {props.token.symbol}
-      <span className="badge bg-primary-light rounded-pill">
+      <span className="badge bg-primary-light">
         {props.token.balance.toFixed(4)}
-        <br />{" "}
+        <br />
         {(props.token.balance * props.token.tokenData.current_price).toFixed(2)}
         $
+        <br />
+        {`1 = ${props.token.tokenData.current_price}$`}
       </span>
     </li>
   );
