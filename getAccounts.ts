c268d004e288f5233428eda5ca8b80ea84eb8d3e
Balazs Nyirfa <balazsnyirfa@gmail.com>
2021-12-20 20:25:05 +0100

    add support for unclaimed thor rewards

--- getAccounts.ts
+++ getAccounts.ts
@@ -6,28 +6,13 @@ import {
   Account,
   BinanceAccount,
 } from "./models/Account";
-import { ERC20Abi, thorAbi } from "./models/ContractABI";
+import { ERC20, strongV1, strongV2 } from "./models/ContractABI";
 // web3
-import { ethers } from "ethers";
-import { Interface } from "ethers/lib/utils";
-const web3Provider = new ethers.providers.JsonRpcProvider(
-  process.env.ethNodeURL
-);
+import Web3 from "web3";
+const web3 = new Web3(process.env.ethNodeURL);
 
-export const fetchCryptoData = async (
-  name: string,
-  symbol: string,
-  currency: string
-) => {
+export const fetchCryptoData = async (name: string, symbol: string, currency: string) => {
   try {
-    // rename token names to match CoinGecko the API id
-    if (name === "MEMOries") {
-      name = "Wonderland";
-    }
-    if (name === "THOR v2") {
-      name = "thor";
-    }
-
     const coingeckoAPI = "https://api.coingecko.com/api/v3";
     const fetchURLByName = `${coingeckoAPI}/coins/markets?vs_currency=${currency}&ids=${name.toLowerCase()}`;
     const resByName = await fetch(fetchURLByName);
@@ -62,20 +47,16 @@ export const getPopulatedEthAccounts = async (
     for (const ethAccount of ethAccounts) {
       const populatedEthAccount: EthAccount = {
         value: ethAccount.value,
-        tokens: await getTokenBalances(
-          ethAccount.value,
-          contractAccounts,
-          currency
-        ),
+        tokens: await getTokenBalances(ethAccount.value, contractAccounts, currency),
         id: ethAccount.id,
       };
-      // add AVAX as the 1st token in the tokens array
+      // add ETH as the 1st token in the tokens array
       const ethToken: Token = {
         balance: await getEthAddressBalance(ethAccount.value),
-        name: "avalanche-2",
-        symbol: "AVAX",
+        name: 'ethereum',
+        symbol: 'ETH',
         decimals: 18,
-        tokenData: await fetchCryptoData("avalanche-2", "AVAX", currency),
+        tokenData: await fetchCryptoData('ethereum', 'ETH', currency)
       };
       populatedEthAccount.tokens.unshift(ethToken);
       populatedEthAccounts.push(populatedEthAccount);
@@ -91,31 +72,38 @@ export const getPopulatedEthAccounts = async (
 // get balance of an eth address
 const getEthAddressBalance = async (ethAccountAddress: string) => {
   try {
-    const balanceInWei = await web3Provider.getBalance(ethAccountAddress);
-    const balance = parseFloat(ethers.utils.formatEther(balanceInWei));
+    const balanceInWei = await web3.eth.getBalance(ethAccountAddress);
+    const balance = parseFloat(web3.utils.fromWei(balanceInWei));
     return balance;
   } catch (error) {
     console.error(error);
   }
 };
 
-// get unclaimed thor reward
-const getUnclaimedThorReward = async (ethAccountAddress: string, contractAddress: string) => {
+// get unclaimed strong reward from strong proxy contract
+const getUnclaimedStrongReward = async (ethAccountAddress: string) => {
   try {
-    // create void signer as 'msg.sender' is needed
-    const voidSigner = new ethers.VoidSigner(ethAccountAddress, web3Provider);
-    const thorContractInstance = new ethers.Contract(
-      contractAddress,
-      thorAbi,
-      voidSigner
-    );
-    const unclaimedRewardRaw = await thorContractInstance.getRewardAmount();
-    const unclaimedReward = Number.parseFloat(ethers.utils.formatUnits(unclaimedRewardRaw, 18));
-    return unclaimedReward;
+    const strongProxyContractV1 = '0xFbdDaDD80fe7bda00B901FbAf73803F2238Ae655';
+    const strongProxyContractV2 = '0xc5622f143972a5da6aabc5f5379311ee5eb48568';
+    const V1contractInstance = new web3.eth.Contract(strongV1, strongProxyContractV1);
+    const V2contractInstance = new web3.eth.Contract(strongV2, strongProxyContractV2);
+    const currentBlock = await web3.eth.getBlockNumber();
+    const rawRewardsFromV1: number = await V1contractInstance.methods.getRewardAll(ethAccountAddress, currentBlock).call();
+
+    // get rewards from V2 contract
+    const entityNodeCount: number = await V2contractInstance.methods.entityNodeCount(ethAccountAddress).call();
+    let rawRewardsFromV2 = 0;
+    for (let nodeId = 1; nodeId <= entityNodeCount; nodeId++) {
+      const rawRewardOfNodeId: number = await V2contractInstance.methods.getNodeReward(ethAccountAddress, nodeId).call();
+      rawRewardsFromV2 += (rawRewardOfNodeId * (10 ** -18));
+    }
+
+    const unclaimedRewards = ((rawRewardsFromV1 * (10 ** -18)) + rawRewardsFromV2)
+    return unclaimedRewards;
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 // get token balances of an eth address
 const getTokenBalances = async (
@@ -126,34 +114,23 @@ const getTokenBalances = async (
   try {
     const tokens: Token[] = [];
     for (const contract of contracts) {
-      const contractInstance = new ethers.Contract(
-        contract.value,
-        ERC20Abi,
-        web3Provider
-      );
-      // query token data
-      const name: string = await contractInstance.name();
-      const symbol: string = await contractInstance.symbol();
-      const decimal: number = await contractInstance.decimals();
-      const rawBalance: number = await contractInstance.balanceOf(
-        ethAccountAddress
-      );
+      // query token info
+      const contractInstance = new web3.eth.Contract(ERC20, contract.value);
+      const name = await contractInstance.methods.name().call();
+      const symbol = await contractInstance.methods.symbol().call();
       const token: Token = {
         name: name,
         symbol: symbol,
-        balance: Number.parseFloat(
-          ethers.utils.formatUnits(rawBalance, decimal)
-        ),
-        decimals: await contractInstance.decimals(),
+        balance:
+          (await contractInstance.methods.balanceOf(ethAccountAddress).call()) /
+          10 ** (await contractInstance.methods.decimals().call()),
+        decimals: await contractInstance.methods.decimals().call(),
         tokenData: await fetchCryptoData(name, symbol, currency),
       };
 
-      // if token is THOR, then add the unclaimed tokens from the contract
-      if (symbol === "THOR") {
-        token.balance += await getUnclaimedThorReward(
-          ethAccountAddress,
-          contract.value
-        );
+      // if token is STRONG, then add the unclaimed tokens from the contract
+      if (symbol === 'STRONG') {
+        token.balance += await getUnclaimedStrongReward(ethAccountAddress);
       }
 
       tokens.push(token);
